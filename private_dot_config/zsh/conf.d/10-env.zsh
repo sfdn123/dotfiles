9dc9f569902667d7bac38d12f1d4de4d6e38bed7
sfdn123 <shogo.imao@gmail.com>
2026-04-07 12:55:26 +0900

    update: for ubuntu

--- private_dot_config/zsh/conf.d/10-env.zsh
+++ private_dot_config/zsh/conf.d/10-env.zsh
@@ -2,3 +2,17 @@ export LANG="${LANG:-en_US.UTF-8}"
 export EDITOR="nvim"
 export VISUAL="nvim"
 WORDCHARS=''
+
+# Initialize Homebrew from common install locations when available.
+for brew_bin in \
+    "/opt/homebrew/bin/brew" \
+    "/usr/local/bin/brew" \
+    "/home/linuxbrew/.linuxbrew/bin/brew"
+do
+    if [ -x "$brew_bin" ]; then
+        eval "$("$brew_bin" shellenv)"
+        break
+    fi
+done
+
+unset brew_bin
