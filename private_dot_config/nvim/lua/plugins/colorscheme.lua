268d5f07dc22b4b4e5c33d7fe537ef00f8ae93c8
sfdn123 <shogo.imao@gmail.com>
2026-04-14 01:20:28 +0900

    change theme to original

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,18 @@ return {
     "navarasu/onedark.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "darker",
-    },
-    config = function(_, opts)
-      require("onedark").setup(opts)
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+        highlights = {
+          ["@property"] = { fg = "$fg" },
+          ["@variable.member"] = { fg = "$fg" },
+          ["@variable.parameter.builtin"] = { fg = "$red" },
+          ["@lsp.type.enumMember"] = { fg = "$fg" },
+          ["@lsp.typemod.variable.static"] = { fg = "$fg" },
+          ["@lsp.typemod.variable.defaultLibrary"] = { fg = "$fg" },
+        },
+      })
       require("onedark").load()
     end,
   },
