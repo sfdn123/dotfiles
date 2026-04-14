2ee3fc66b3a05dd378f0bb0eeb67deb1355ff9e9
sfdn123 <shogo.imao@gmail.com>
2026-04-14 23:57:45 +0900

    change: theme

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
--- theme: OneDark-Pro Night Flat
+-- colors overridden to match Cursor "One Dark Pro Darker" exactly
 return {
   {
     "navarasu/onedark.nvim",
@@ -7,19 +7,32 @@ return {
     priority = 1000,
     config = function()
       require("onedark").setup({
-        style = "dark",
+        style = "darker",
         colors = {
-          bg0 = "#16191d",
-          bg1 = "#1e2227",
-          bg2 = "#2c313c",
-          bg3 = "#3e4452",
-          bg_d = "#111417",
+          bg0 = "#23272e",
+          bg1 = "#2c313a",
+          bg2 = "#323844",
+          bg3 = "#3e4451",
+          bg_d = "#1e2227",
+          fg = "#abb2bf",
+          red = "#e06c75",
+          orange = "#d19a66",
+          yellow = "#e5c07b",
+          green = "#98c379",
+          cyan = "#56b6c2",
+          blue = "#61afef",
+          purple = "#c678dd",
+          grey = "#5c6370",
         },
         highlights = {
-          ["@property"] = { fg = "$fg" },
+          ["@variable"] = { fg = "$fg" },
           ["@variable.member"] = { fg = "$fg" },
+          ["@variable.builtin"] = { fg = "$red" },
+          ["@variable.parameter"] = { fg = "$red" },
           ["@variable.parameter.builtin"] = { fg = "$red" },
+          ["@property"] = { fg = "$fg" },
           ["@lsp.type.enumMember"] = { fg = "$fg" },
+          ["@lsp.type.variable"] = { fg = "$fg" },
           ["@lsp.typemod.variable.static"] = { fg = "$fg" },
           ["@lsp.typemod.variable.defaultLibrary"] = { fg = "$fg" },
         },
