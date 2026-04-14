471c32bc790a115328f3628a899ede8d1f02985e
sfdn123 <shogo.imao@gmail.com>
2026-04-14 10:16:44 +0900

    change: font

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
+-- theme: OneDark-Pro Night Flat
 return {
   {
     "navarasu/onedark.nvim",
@@ -6,7 +7,14 @@ return {
     priority = 1000,
     config = function()
       require("onedark").setup({
-        style = "darker",
+        style = "dark",
+        colors = {
+          bg0 = "#16191d",
+          bg1 = "#1e2227",
+          bg2 = "#2c313c",
+          bg3 = "#3e4452",
+          bg_d = "#111417",
+        },
         highlights = {
           ["@property"] = { fg = "$fg" },
           ["@variable.member"] = { fg = "$fg" },
